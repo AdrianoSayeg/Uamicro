0db6f9f440c84e634f9c6eb8d35332a701f4a94c
AdrianoSayeg <adrianosayeg@gmail.com>
2026-01-27 00:43:30 -0600

    UAMICRO 1: Nueva version con interfaz gráfica actualizada y correccion de los componentes

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5 import QtWidgets
-from interfaz import uamicro1
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    window = uamicro1()
-    window.show()
-    sys.exit(app.exec_())
